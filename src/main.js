5ad2d58b98752bd4e4191999399b6902ea5f7414
fgf <7805864+fgf2021@user.noreply.gitee.com>
2020-09-23 16:44:57 +0800

    商品分类功能开发完成

--- src/main.js
+++ src/main.js
@@ -3,9 +3,10 @@ import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
 import './assets/css/global.css'
+import TreeTable from 'vue-table-with-tree-grid'
 import axios from 'axios'
 
-axios.defaults.baseURL = 'https://www.liulongbin.top:8888/api/private/v1/'
+axios.defaults.baseURL = 'http://www.ysqorz.top:8888/api/private/v1/'
 axios.interceptors.request.use(config=>{
   config.headers.Authorization = window.sessionStorage.getItem('token');
   return config;
@@ -13,7 +14,7 @@ axios.interceptors.request.use(config=>{
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
-
+Vue.component('tree-table',TreeTable);
 new Vue({
   router,
   render: h => h(App)
